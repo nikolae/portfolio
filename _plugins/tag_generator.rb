90f19aa4dcdf2fc85a2afd91b182b6af04021c8e
nikolae <nikolae@users.noreply.github.com>
2023-07-05 21:30:03 -0600

    Pushing dev to live

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -25,7 +25,21 @@ def generate_tag_file(tag)
     </li>
   {% endif %}
 {% endfor %}
-</ul>"
+</ul>
+<p>
+			<h4>All Tags</h4>
+			{% capture temptags %}
+			  {% for tag in site.tags %}
+				{{ tag[1].size | plus: 1000 }}\#{{ tag[0] }}\#{{ tag[1].size }}
+			  {% endfor %}
+			{% endcapture %}
+			{% assign sortedtemptags = temptags | split:' ' | sort | reverse %}
+			{% for temptag in sortedtemptags %}
+			  {% assign tagitems = temptag | split: '#' %}
+			  {% capture tagname %}{{ tagitems[1] }}{% endcapture %}
+			  <a href=\"/tags/{{ tagname }}.html\"><code class=\"highligher-rouge\"><nobr>{{ tagname }}</nobr></code></a>
+			{% endfor %}
+			</p>"
     end
     # generate feed file
     # File.open("feeds/#{tag}.xml", "wb") do |file|
--- pages/tags/FPGA.md
+++ pages/tags/FPGA.md
@@ -19,3 +19,17 @@ tags: FPGA
   {% endif %}
 {% endfor %}
 </ul>
+<p>
+			<h4>All Tags</h4>
+			{% capture temptags %}
+			  {% for tag in site.tags %}
+				{{ tag[1].size | plus: 1000 }}#{{ tag[0] }}#{{ tag[1].size }}
+			  {% endfor %}
+			{% endcapture %}
+			{% assign sortedtemptags = temptags | split:' ' | sort | reverse %}
+			{% for temptag in sortedtemptags %}
+			  {% assign tagitems = temptag | split: '#' %}
+			  {% capture tagname %}{{ tagitems[1] }}{% endcapture %}
+			  <a href="/tags/{{ tagname }}.html"><code class="highligher-rouge"><nobr>{{ tagname }}</nobr></code></a>
+			{% endfor %}
+			</p>
